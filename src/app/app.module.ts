42b352e8ffb28082b215045732072e94f971fb60
Remi <42407006+GuysmoB@users.noreply.github.com>
2022-07-21 09:53:01 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,13 +17,6 @@ import * as Candy from 'fusioncharts/themes/fusioncharts.theme.candy';
 
 FusionChartsModule.fcRoot(FusionCharts, Charts, FusionTheme, TimeSeries, Candy);
 const firebaseConfig = {
-  apiKey: 'AIzaSyAn1Jn7J-vumyC-XL1LY7AJEq7R_oqUGdc',
-  authDomain: 'real-time-trading-bot.firebaseapp.com',
-  databaseURL: 'https://real-time-trading-bot.firebaseio.com',
-  projectId: 'real-time-trading-bot',
-  storageBucket: 'real-time-trading-bot.appspot.com',
-  messagingSenderId: '711877653368',
-  appId: '1:711877653368:web:5c5e1ebab700ab23624e8d',
 };
 
 @NgModule({
